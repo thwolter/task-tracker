e2341efab35d35ecc551a1fb120f8f5a0a32af5f
Thomas Wolter <thwolter@gmail.com>
2026-09-06 16:06:01 +0200

    feat(ci): add OS-specific release asset preparation and include Windows icon

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
 fn main() {
     slint_build::compile("ui/app-window.slint").expect("Failed to compile SLint UI");
+
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("resources/icon.ico");
+        res.compile().expect("Failed to embed Windows icon");
+    }
 }
