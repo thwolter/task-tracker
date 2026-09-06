577633eff3d90f50e247f9330ff9630f1ab9b2a6
Thomas Wolter <thwolter@gmail.com>
2026-09-06 17:36:55 +0200

    feat(build): relocate Windows icon and update resources in build script

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     #[cfg(target_os = "windows")]
     {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("resources/icon.ico");
+        res.set_icon("resources/icons/icon.ico");
         res.compile().expect("Failed to embed Windows icon");
     }
 }
